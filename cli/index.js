235f34b6eaa6671ba42a656c50a5e1ac6025c94e
Aditya Narayan Mishra <mishraaditya384@gmail.com>
2021-09-24 21:26:54 +0530

    Removed extra parameters from the commands, Errors will be shown directly

--- cli/index.js
+++ cli/index.js
@@ -6,13 +6,14 @@ const inquirer = require('inquirer')
 const chalk = require('chalk');
 const isUrlValid = require("./utils/isValidUrl")
 const validateEmail = require("./utils/checkEmail")
-
+require("dotenv").config();
 const omsPath = path.join(os.homedir(), ".oms")
 
 const axios = require("axios");
 
 
-const createResults = async url => {
+const createResults = async () => {
+    const createURL = "http://localhost:8080/post";
     const questions = [
         {
             "message": "Enter Title: ",
@@ -34,12 +35,12 @@ const createResults = async url => {
     // console.log(answers);
     fs.readFile(path.join(omsPath, "context.json"), "utf8", async (err, jsonString) => {
         if (err) {
-            console.log(chalk.yellowBright("Error reading file from disk: Please Login Again!"));
+            console.log(chalk.yellowBright("Session Expired: Please Login Again!"));
             return;
         }
         try {
             const data = JSON.parse(jsonString);
-            const res = await axios.post(url, answers, { headers: { 'authorization': `Bearer ${data?.access_token}` } });
+            const res = await axios.post(createURL, answers, { headers: { 'authorization': `Bearer ${data?.access_token}` } });
             console.log((res.data))
         } catch (err) {
             if (err?.response?.status === 401)
@@ -52,41 +53,37 @@ const createResults = async url => {
 
 }
 
-const getResults = async url => {
+const getResults = async (id = null) => {
+    const getURL = 'http://localhost:8080/posts'
     fs.readFile(path.join(omsPath, "context.json"), "utf8", async (err, jsonString) => {
         if (err) {
-            console.log(chalk.yellowBright("Error reading file from disk: Please Login Again!"));
+            console.log(chalk.yellowBright("Session Expired. Please Login Again!"));
             return;
         }
         try {
             const data = JSON.parse(jsonString);
-            const res = await axios.get(url, { headers: { 'authorization': `Bearer ${data?.access_token}` } });
-            console.log(res.data)
+            const headers = { 'authorization': `Bearer ${data?.access_token}` };
+            let response;
+            if (id)
+                response = await axios.get(`${getURL}/${id}`, { headers })
+            else
+                response = await axios.get(getURL, { headers });
+            console.log(response.data)
         } catch (err) {
             if (err?.response?.status === 401)
                 console.log(chalk.redBright("401 Unauthorized Error"));
-            else if (err?.response?.status === 404)
-                console.log(chalk.red("URL not found! Please input correct URL."))
+            else if (err.code === "ECONNREFUSED")
+                console.log(chalk.red("Unable to connect to the Server"));
             else
-                console.log(chalk.red("Error parsing JSON string:"));
+                console.log(chalk.red("Error parsing JSON string"));
 
         }
     });
 }
 
-const login = async (u, a, d) => {
+const login = async (email, api) => {
+
     let questions = [
-        {
-            type: 'input',
-            name: 'client',
-            message: "Client ID: "
-        },
-        {
-            type: 'password',
-            name: 'secret',
-            message: "Client Secret ID: ",
-            mask: true
-        },
         {
             type: 'password',
             message: 'Enter password: ',
@@ -98,19 +95,29 @@ const login = async (u, a, d) => {
     const answers = await inquirer.prompt(questions);
     const data = {
         grant_type: 'password',
-        client_id: answers['client'],
-        client_secret: answers['secret'],
-        audience: d,
-        username: u,
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: api,
+        username: email,
         password: answers['password'],
         header: "content-type: application/x-www-form-urlencoded",
     }
 
-    await axios.post(a, data).then(res => {
+    await axios.post(`${process.env.AUTH0_DOMIN}/oauth/token`, data).then(res => {
         fs.writeFile(path.join(omsPath, "context.json"), JSON.stringify(res.data), function (err) {
-            if (err) throw err; console.log(chalk.bold.blueBright(`Context saved in ${path.join(omsPath, "context.json")}`));
+            if (err) throw err;
+            console.log(chalk.bold.blueBright(`Context saved in ${path.join(omsPath, "context.json")}`));
         });
     }).catch(err => {
+        // console.log(err.response);
+        if (err.response) {
+            const { status, data } = err.response;
+            if (status) {
+                console.log(chalk.red(data.error));
+                console.log(chalk.red(status + " " + data.error_description));
+            }
+
+        }
         console.log(chalk.red("Please check the URL or Email Address"));
     })
 
@@ -127,21 +134,16 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
             username: {
                 alias: 'u',
                 demandOption: true
-
             },
-            auth_url: {
+            api_url: {
                 alias: 'a',
-                demandOption: true
-            },
-            audience_url: {
-                alias: 'd',
                 demandOption: true
             }
         },
         handler: async (argv) => {
 
-            if (validateEmail(argv.username) && isUrlValid(argv.auth_url) && isUrlValid(argv.audience_url)) {
-                await login(argv.u, argv.auth_url, argv.audience_url);
+            if (validateEmail(argv.username) && isUrlValid(argv.api_url)) {
+                await login(argv.username, argv.api_url);
             }
             else
                 console.log(chalk.red("There is/are some error/s in input. Please check them"))
@@ -151,50 +153,43 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
         command: "get",
         desc: "Get the Status",
         builder: {
-            url: {
-                alias: 'u',
-                demandOption: true
+            id: {
+                alias: 'i',
+                demandOption: false
             }
         },
         handler: async (argv) => {
-            if (isUrlValid(argv?.url)) {
-                try {
-                    await getResults(argv?.url);
-                } catch (error) {
-                    if (error?.response?.status === 401)
-                        console.log(chalk.red("Unauthorized Access. Login Again!!"))
 
-                    else if (error?.code === "ECONNREFUSED")
-                        console.log(chalk.red("URL not Found"))
-                }
-            } else {
-                console.log(chalk.red("There is/are some error/s in input. Please check them"))
+            try {
+                await getResults(argv.id);
+            } catch (error) {
+                if (error?.response?.status === 401)
+                    console.log(chalk.red("Unauthorized Access. Login Again!!"))
+
+                else if (error?.code === "ECONNREFUSED")
+                    console.log(chalk.red("URL not Found"))
             }
+            // } else {
+            //     console.log(chalk.red("There is/are some error/s in input. Please check them"))
+            // }
 
         },
     })
     .command({
         command: "create",
         desc: "Create POST Requests",
-        builder: {
-            url: {
-                alias: 'u',
-                demandOption: true
+        handler: async () => {
+            try {
+                await createResults();
+            } catch (error) {
+                if (error?.response?.status === 401)
+                    console.log(chalk.red("Unauthorized Access. Login Again!!"))
+                else if (error?.code === "ECONNREFUSED")
+                    console.log(chalk.red("URL not Found"))
             }
-        },
-        handler: async (argv) => {
-            if (validateEmail(argv?.url)) {
-                try {
-                    await createResults(argv?.url);
-                } catch (error) {
-                    if (error?.response?.status === 401)
-                        console.log(chalk.red("Unauthorized Access. Login Again!!"))
-                    else if (error?.code === "ECONNREFUSED")
-                        console.log(chalk.red("URL not Found"))
-                }
-            } else {
-                console.log(chalk.red("Please check URL again!!!"));
-            }
+            // } else {
+            //     console.log(chalk.red("Please check URL again!!!"));
+            // }
 
         },
     })
